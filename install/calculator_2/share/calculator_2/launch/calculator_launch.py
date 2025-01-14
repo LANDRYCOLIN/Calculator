986ee007300b17ee43f108622a06b70d972d086a
LANDRYCOLIN <1874646570@qq.com>
2025-01-14 15:54:27 +0800

    Launch verion 4

--- install/calculator_2/share/calculator_2/launch/calculator_launch.py
+++ install/calculator_2/share/calculator_2/launch/calculator_launch.py
@@ -1,19 +1,23 @@
 import launch
 from launch import LaunchDescription
-from launch_ros.actions import Node  # 导入 Node 组件
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument('param', default_value='value', description='A parameter'),
+
         Node(
             package='calculator_2',
             executable='calculator_publisher',
-            name='calculator_publisher',
+            name='calculator_publisher_node',
             output='screen'
         ),
+        
         Node(
             package='calculator_2',
             executable='calculator_subscriber',
-            name='calculator_subscriber',
+            name='calculator_subscriber_node',
             output='screen'
         ),
     ])
--- launch/calculator_launch.py
+++ launch/calculator_launch.py
@@ -1,19 +1,23 @@
 import launch
 from launch import LaunchDescription
-from launch.actions import Node
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument('param', default_value='value', description='A parameter'),
+
         Node(
-            package='calculator_2', 
+            package='calculator_2',
             executable='calculator_publisher',
-            name='calculator_publisher_node', 
+            name='calculator_publisher_node',
             output='screen'
         ),
+        
         Node(
             package='calculator_2',
             executable='calculator_subscriber',
             name='calculator_subscriber_node',
             output='screen'
-        )
+        ),
     ])
